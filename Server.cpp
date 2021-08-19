8e864eb4ce80594a2207276be9c71bec2e562da2
hyoukim <dkdemfp2@naver.com>
2021-08-20 00:16:52 +0900

    main thread server test

--- Server.cpp
+++ Server.cpp
@@ -44,7 +44,7 @@ struct kevent *Server::getEvent_list(void)
 	return (this->event_list);
 }
 
-void Server::init_Server()
+void Server::init_Server(int port)
 {
 	if ((this->serv_sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
 		exit_with_perror("socket() error!!!");
@@ -52,7 +52,7 @@ void Server::init_Server()
 	memset(&this->serv_adr, 0, sizeof(this->serv_adr));
 	this->serv_adr.sin_family = AF_INET;
 	this->serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	this->serv_adr.sin_port = htons(8080);
+	this->serv_adr.sin_port = htons(port);
 
 	if (::bind(this->serv_sock, (struct sockaddr *)&(this->serv_adr), sizeof(this->serv_adr)) == -1)
 		exit_with_perror("bind() error!!!");
@@ -94,6 +94,71 @@ void Server::accept_new_client(void)
 	this->add_kevent(clnt_sock, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
 	this->add_kevent(clnt_sock, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
 	clients[clnt_sock] = "";
+}
+
+void Server::run(void)
+{
+	cout << "run!" << endl;
+	int new_events;
+	struct kevent *curr_event;
+	while(1)
+	{
+		new_events = kevent(this->kq, &(this->change_list[0]), this->change_list.size(),
+			this->event_list, 8, NULL);
+		if (new_events == -1)
+			exit_with_perror("kevent() error!!!");
+		
+		this->change_list.clear();
+
+		for (int i = 0; i < new_events; ++i)
+		{
+			curr_event = &(this->event_list[i]);
+
+			if (curr_event->flags & EV_ERROR)
+			{
+				if (curr_event->ident == this->serv_sock)
+					exit_with_perror("server socket error!!!");
+				else
+					disconnected_client(curr_event->ident);
+			}
+			else if (curr_event->filter == EVFILT_READ)
+			{
+				if (curr_event->ident == this->serv_sock)
+					accept_new_client();
+				else if (this->clients.find(curr_event->ident) != this->clients.end())
+				{
+					char req_line[1024];
+					char method[10];
+					char ct[15];
+					char file_name[30];
+			
+					recv(curr_event->ident, req_line, 1023, 0);	
+					cout << req_line << endl;
+
+					if (strstr(req_line, "HTTP/") == NULL)
+					{
+						cerr << "Not HTTP protocol" << endl;
+						disconnected_client(curr_event->ident);
+						return ;
+					}
+
+					strcpy(method, strtok(req_line, " /"));
+					cout << "method : " << method << endl;
+					strcpy(file_name, strtok(NULL, " /"));
+					cout << "filename : " << file_name << endl;
+					strcpy(ct, content_type(file_name));
+					cout << "content_type : " << ct << endl;
+					if (strcmp(method, "GET") != 0)
+					{
+						cerr << "Method is not GET!!!" << endl;
+						disconnected_client(curr_event->ident);
+						return ;
+					}
+					send_data(curr_event->ident, ct, file_name);
+				}
+			}
+		}
+	}
 }
 
 void exit_with_perror(const string &msg)
--- Server.hpp
+++ Server.hpp
@@ -38,12 +38,16 @@ public:
 	vector<struct kevent> &getChange_list(void);
 	struct kevent *getEvent_list(void);
 
-	void init_Server();
+	void init_Server(int port);
 	void add_kevent(uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags, uintptr_t data, void *udata);
 	void disconnected_client(int clnt_fd);
 	void accept_new_client(void);
+	void run(void);
 };
 
 void exit_with_perror(const string &msg);
+char *content_type(char *file);
+void send_data(int clnt_sock, char *ct, char *file_name);
+void send_error(int clnt_sock);
 
 #endif 
